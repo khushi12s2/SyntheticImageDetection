0fc9e806e0b560b5b84d396985645fb02c7dfa6d
khushi12s2 <shoumya.sundaram.aiml21@ggits.net>
2024-11-29 01:08:49 +0530

    update

---
+++ src/models.py
@@ -0,0 +1,26 @@
+# src/model.py
+import torch.nn as nn
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 32 * 32, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
+            nn.Sigmoid()  # Sigmoid for binary classification (real vs synthetic)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.fc_layers(x)
+        return x
